65b34f1656b558613541dccdfe909f4680abb778
CauZy-Goes <cauafariasdev@gmail.com>
2025-06-23 18:44:47 -0300

    feat tailwind

--- next-tailwind/src/app/2_tipografia/page.tsx
+++ next-tailwind/src/app/2_tipografia/page.tsx
@@ -9,7 +9,7 @@ export default function Responsividade(){
             ">Título Responsivo</h1>
             <h1 className="font-sans">Título 01</h1>
             <h1 className="font-serif">Título 02</h1>
-            <h1 className="font-mono">Título 03</h1>
+            <h1 className="font-mono">Título 03</h1> 
             <h1 className="text-2xl">Título 04</h1>
 
             <h1 className="italic">Título 06</h1>
@@ -23,3 +23,16 @@ export default function Responsividade(){
         </div>
     )
 }
+
+{/* <h1 className="font-sans">Título 01</h1>            
+            <h1 className="font-serif">Título 02</h1>
+            <h1 className="font-mono">Título 03</h1> 
+            <h1 className="text-2xl">Título 04</h1>
+
+            <h1 className="italic">Título 06</h1>
+            <h1 className="font-thin">Título 06</h1>
+            <h1 className="font-thin tracking-wider">Título 06</h1>
+
+            <h1 className="text-left">Esquerda</h1>
+            <h1 className="text-center">centro</h1>
+            <h1 className="text-right">Direta</h1> */}
